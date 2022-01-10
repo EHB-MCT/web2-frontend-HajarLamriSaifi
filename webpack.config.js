1f20bb8751b2e6058a2c13cac5ecb4ec578ca9f2
HajarLamriSaifi <56121110+HajarLamriSaifi@users.noreply.github.com>
2022-01-10 20:59:32 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+
 module.exports = {
-    entry: './src/script.js',
+    entry: [
+        './src/script.js',
+        './src/item.js',
+        './src/museum.js',
+        './src/saveditems.js',
+    
+    ],
+
     output: {
-        filename: 'main.js',
+        filename: 'webpack.js',
         path: path.resolve(__dirname, 'docs'),
     },
-    mode: 'development',
-
+    mode: 'production',
+    watch: true
 };
